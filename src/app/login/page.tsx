ad83a9c3f7f3d3a8f9d89f64fd82ecb7335f8e13
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-04-22 16:46:32 -0300

    chore: add prettier setup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 const Login = () => {
-    return ( 
-        <>
-            <h1>Login</h1>
-            <Button variant={"default"}>
-                Teste
-            </Button>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <h1>Login</h1>
+      <Button variant={'default'}>Teste</Button>
+    </>
+  );
+};
+
 export default Login;
