6a68d23c8a7433a2c1eee2056c650516ab8b05d6
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-04-22 16:43:30 -0300

    chore: add shadcn.ui setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Login = () => {
+    return ( 
+        <>
+            <h1>Login</h1>
+            <Button variant={"default"}>
+                Teste
+            </Button>
+        </>
+     );
+}
+ 
+export default Login;
