f8dc4a5439a20d85f11f626ba9de0c1450b48084
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-06-04 10:29:43 -0300

    test: removendo linha de teste

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,6 @@ export default async function DashboardLayout({ children }: { children: React.Re
     <div className="flex h-screen overflow-hidden text-white">
       {/* SIDEBAR DESKTOP */}
       <Sidebar username={user.name} />
-      <h1>Teste em dev</h1>
       {/* {children} */}
 
       <div className="flex flex-1 flex-col overflow-hidden">
