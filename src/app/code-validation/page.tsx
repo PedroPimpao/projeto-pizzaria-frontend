71a62f99ab2dd3030c45191e6a7772619c676c26
Pedro Pimpão <pedrohmpimpao@gmail.com>
2026-07-04 14:51:55 -0300

    fix: fixing build errors

--- src/app/code-validation/page.tsx
+++ src/app/code-validation/page.tsx
@@ -1,11 +1,14 @@
 import { CodeValidationForm } from '@/components/forms/code-validation-form';
+import { Suspense } from 'react';
 
 const CodeValidationPage = () => {
   return (
     <>
       <div className="bg-app-background flex min-h-screen items-center justify-center px-4 py-8">
         <div className="w-full">
-          <CodeValidationForm />
+          <Suspense fallback={<div className="text-center text-white">Carregando...</div>}>
+            <CodeValidationForm />
+          </Suspense>
         </div>
       </div>
     </>
--- src/app/recover-access/page.tsx
+++ src/app/recover-access/page.tsx
@@ -1,11 +1,14 @@
-import { RecoverAccessForm } from "@/components/forms/recover-access-form";
+import { RecoverAccessForm } from '@/components/forms/recover-access-form';
+import { Suspense } from 'react';
 
 const RecoverAccessPage = () => {
   return (
     <>
       <div className="bg-app-background flex min-h-screen items-center justify-center px-4 py-8">
         <div className="w-full">
-          <RecoverAccessForm />
+          <Suspense fallback={<div className="text-center text-white">Carregando...</div>}>
+            <RecoverAccessForm />
+          </Suspense>
         </div>
       </div>
     </>
